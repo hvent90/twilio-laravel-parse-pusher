7d6e4c9167a09f9e4d4b788b0971512b9cc3533c
Henry Ventura <hvent90@gmail.com>
2015-05-10 11:44:22 -0400

    Added active styling on left hand nav bar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -4,11 +4,13 @@
 	</div>
 	<div class="collection no-margin">
     @if ($senders)
-  		@foreach ($senders as $sender)
-        @if ($sender['unread'] == true)
-          <a href="/chat/{{ $sender['parse_object_id'] }}" class="collection-item waves-effect waves-light waves-green unread">{{ $sender['phone_number'] }}</a>
+  		@foreach ($senders as $senderDude)
+        @if ($senderDude['phone_number'] == $sender['phone_number'])
+          <a href="/chat/{{ $senderDude['parse_object_id'] }}" class="collection-item waves-effect waves-light waves-green active">{{ $senderDude['phone_number'] }}</a>
+        @elseif ($senderDude['unread'] == true)
+          <a href="/chat/{{ $senderDude['parse_object_id'] }}" class="collection-item waves-effect waves-light waves-green unread">{{ $senderDude['phone_number'] }}</a>
         @else
-          <a href="/chat/{{ $sender['parse_object_id'] }}" class="collection-item waves-effect waves-light waves-green">{{ $sender['phone_number'] }}</a>
+          <a href="/chat/{{ $senderDude['parse_object_id'] }}" class="collection-item waves-effect waves-light waves-green">{{ $senderDude['phone_number'] }}</a>
         @endif
   		@endforeach
     @endif
